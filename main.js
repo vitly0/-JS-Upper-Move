385ffcbedcea17d62b2e63e5ab2493f4a95a9979
EC2 Default User <ec2-user@ip-172-31-8-59.ap-northeast-1.compute.internal>
2022-10-09 08:04:33 +0000

    Page Up

---
+++ main.js
@@ -0,0 +1,24 @@
+$(function() {
+  $('#mainCanvas').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
+  });
+});
+$(document).ready(function() {
+	'use strict';
+	paper.install(window);
+	paper.setup(document.getElementById('mainCanvas'));
+	let c = Shape.Circle(50,50,50);
+	c.fillColor = 'rgba(102,255,51,0.5)';
+	let text = new PointText(50,55) //テキストを中央に
+    text.fillColor = '#000033';
+    text.fontSize = '15';
+	text.fontWeight = "bold"; //文字の太さ
+    text.justification = 'center'; //センタリング
+    text.content = 'ページ上部'; //文字列
+	paper.view.draw
+});
+
+
